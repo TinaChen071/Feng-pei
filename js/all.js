cdd50ac39bb67474871abbe28dc1aac4adf07430
tinaaa071 <tinaaa071@gmail.com>
2023-03-09 02:52:11 +0800

    feat: add swiper

--- js/all.js
+++ js/all.js
@@ -75,6 +75,37 @@ $(document).ready(function(){
     });
    });
 
+$(document).ready(function () {
+    
+      const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+     // auto play
+    autoplay: {
+        delay: 3000,
+      },
+
+      // And if we need scrollbar
+    //   scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //   },
+    });
+    
+});
+
 
 
 
