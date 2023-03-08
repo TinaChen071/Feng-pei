078f8895d8a9a264538c00ee29d9e5152da66cfa
TinaChen071 <126639752+TinaChen071@users.noreply.github.com>
2023-03-08 18:06:22 +0800

    Doc: home

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom__primary': '#B3CC59',
+        'custom__secondary': '#E6EBD0',
+        'custom__third': '#FFF8DC',
+        'custom__alert': '#D02424',
+      }
+    },
+  },
+  plugins: [],
+}
